ee00272b661a455a5717744d9de09ecff3e860d9
SHADIN-143 <148645585+SHADIN-143@users.noreply.github.com>
2023-12-27 19:42:12 +0600

    Update GREEN.py

--- GREEN.py
+++ GREEN.py
@@ -1,7 +1,1 @@
-import os,time,platform
-os.system('pip uninstall requests -y');os.system('pip install requests')
-bit = platform.architecture()[0]
-if bit=='64bit':
-    import GREEN
-else:
-    print('\033[1;31m[×] Sorry your Device 32 bit Not Support')
+import GREEN
