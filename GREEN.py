67136ea59ae7bbda2f28bee1940e36a6c36d2ea4
SHADIN-143 <148645585+SHADIN-143@users.noreply.github.com>
2024-01-16 10:28:36 +0600

    Update GREEN.py

--- GREEN.py
+++ GREEN.py
@@ -3,6 +3,6 @@ os.system('git pull')
 os.system('pip uninstall requests -y');os.system('pip install requests')
 bit = platform.architecture()[0]
 if bit=='64bit':
-    import asd
+    import asd1
 else:
     print('\033[1;31m[×] Sorry your Device 32 bit Not Support')
